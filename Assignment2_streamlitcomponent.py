896a88c2fb330008c7386c64951cc945454244b0
Sean Blamires <69487750+SeanBlamires612@users.noreply.github.com>
2022-03-19 14:33:41 +1100

    Simplified streamlit file

---
+++ Assignment2_streamlitcomponent.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.write(""" 
+#Application
+Predicting quality of *Beer*
+""")
+
+streamlit run c:/Users/z3066824/OneDrive-UNSW/Desktop/Assignment2_streamlitcomponent.py
