20d7723cdf1a861282e4afbf3c193876f99e4b52
Sean Blamires <69487750+SeanBlamires612@users.noreply.github.com>
2022-03-19 15:01:18 +1100

    Update Assignment2_streamlitcomponent2.py

--- Assignment2_streamlitcomponent2.py
+++ Assignment2_streamlitcomponent2.py
@@ -6,4 +6,36 @@ st.write("""
 Predicting quality of *Beers*
 """)
 
+import numpy as np
+import pandas as pd
 
+path = "C:\\Users\\z3066824\\OneDrive - UNSW\Documents\\"
+
+file = "beer_reviews.csv"
+
+df = pd.read_csv(path+file)
+
+df
+
+df.info()
+
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 1586614 entries, 0 to 1586613
+Data columns (total 13 columns):
+ #   Column              Non-Null Count    Dtype  
+---  ------              --------------    -----  
+ 0   brewery_id          1586614 non-null  int64  
+ 1   brewery_name        1586599 non-null  object 
+ 2   review_time         1586614 non-null  int64  
+ 3   review_overall      1586614 non-null  float64
+ 4   review_aroma        1586614 non-null  float64
+ 5   review_appearance   1586614 non-null  float64
+ 6   review_profilename  1586266 non-null  object 
+ 7   beer_style          1586614 non-null  object 
+ 8   review_palate       1586614 non-null  float64
+ 9   review_taste        1586614 non-null  float64
+ 10  beer_name           1586614 non-null  object 
+ 11  beer_abv            1518829 non-null  float64
+ 12  beer_beerid         1586614 non-null  int64  
+dtypes: float64(6), int64(3), object(4)
+memory usage: 157.4+ MB
